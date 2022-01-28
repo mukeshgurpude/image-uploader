6d93269af3c6853df7c4cde0f9d2f4d612a8b59a
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-01-29 00:47:00 +0530

    feat: add loader

--- src/components/app.js
+++ src/components/app.js
@@ -6,16 +6,13 @@ import Loader from './loader'
 import Success from './success'
 
 const App = () => {
-	const [state, setState] = useState(0);
+	const [state, setState] = useState(1);
 	return <div id="app">
 			<Wrapper>
 			{
 				state == 0 ? <Uploader onUpload={() => setState(1)} />
 				: (state == 1 ? <Loader /> : <Success />)
 			}
-			<button onClick={() => {
-				setState(s => s == 2 ? 0 : s + 1);
-			}} children='Click to update' />
 			</Wrapper>
 		</div>
 }
--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,3 +1,10 @@
+import style from './style.css'
+
 export default function Loader() {
-  return <h1>Loading...</h1>
+  return <>
+    <h2>Uploading...</h2>
+    <div className={style.outer}>
+      <div className={style.inner} />
+    </div>
+  </>
 }
