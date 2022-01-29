a722f8327dc0a6e82510daf92106842d727ee2a0
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-01-29 17:24:51 +0530

    feat: rename env variable

--- preact.config.js
+++ preact.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export default (config, env, helpers) => {
     config.plugins.push(
         new helpers.webpack.DefinePlugin({
-          'process.env.BUCKET': JSON.stringify(process.env.URL),
+          'process.env.BUCKET': JSON.stringify(process.env.DB_URL),
           'process.env.API_KEY': JSON.stringify(process.env.PUBLIC_KEY),
         }),
     );
